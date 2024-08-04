51651fd1e5f4cb63803d694f0e14c98f32e54ca3
Saw Kyaw Myint <scm.sawkyawmyint@gmail.com>
2024-08-04 17:29:38 +0630

    add relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'quantity',
+        'price',
+        'cart_number',
+    ];
+
+    /**
+     * Get the user that owns the order.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the product associated with the order.
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -11,8 +12,23 @@ class Product extends Model
 
     protected $fillable = [
         'title',
+        'user_id',
         'description',
         'categories',
+        'total_order',
         'price',
     ];
+
+    /**
+     * Get the user that owns the product.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }

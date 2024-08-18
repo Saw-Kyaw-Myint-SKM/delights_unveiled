cc104b899c570dfec5b23aaf4d261f4be0846786
Zaw Myo Niang <zzawmyonaing001@gmail.com>
2024-08-18 13:03:41 +0630

    update payment method radio design

--- resources/js/Layouts/WelcomeLayout.jsx
+++ resources/js/Layouts/WelcomeLayout.jsx
@@ -55,7 +55,7 @@ export default function Authenticated({
 
     return (
         <div className="min-h-screen bg-gray-100">
-            <nav className="bg-white border-b border-gray-100 bg-white p-4 shadow sticky top-0 z-50">
+            <nav className="bg-white border-b border-gray-100 p-4 shadow sticky top-0 z-50">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="flex justify-center items-center">

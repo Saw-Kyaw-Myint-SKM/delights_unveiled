4014a489af8a4f901b4dd5c0f86ad40e33ea89ad
Saw Kyaw Myint <scm.sawkyawmyint@gmail.com>
2024-08-20 22:47:12 +0630

    add notification

--- resources/js/Pages/Contact.jsx
+++ resources/js/Pages/Contact.jsx
@@ -6,6 +6,7 @@ import TextArea from "@/Components/TextArea";
 import TextInput from "@/Components/TextInput";
 import InputLabel from "@/Components/InputLabel";
 import { useRef } from "react";
+import Swal from "sweetalert2";
 
 import { useForm } from "@inertiajs/react";
 
@@ -31,7 +32,24 @@ export default function Contact({ auth, laravelVersion, phpVersion }) {
         console.log("data", data);
         post(route("contact.store"), {
             preserveScroll: true,
-            onSuccess: () => reset(),
+            onSuccess: () => {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.onmouseenter = Swal.stopTimer;
+                        toast.onmouseleave = Swal.resumeTimer;
+                    },
+                });
+                Toast.fire({
+                    icon: "success",
+                    title: "Mail has been sent successfully.",
+                });
+                reset();
+            },
             onError: (errors) => {
                 console.log("errors", errors);
             },
